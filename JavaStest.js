52d545fa0a4d04ffdda657e258c6724ba0e87dcf
Abhishek Mahato <abhishekmahato3171@gmail.com>
2026-05-26 18:07:14 +0530

    1st JS code

---
+++ JavaStest.js
@@ -0,0 +1,10 @@
+// 1. A simple function that takes a name and returns a greeting
+function createGreeting(name) {
+    return "Hello, " + name + "! Your JavaScript is working perfectly.";
+}
+
+// 2. We store the result of that function in a variable called 'message'
+const message = createGreeting("Abhi");
+
+// 3. This prints the message to your terminal or browser console
+console.log(message);
