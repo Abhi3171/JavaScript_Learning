8a6f208c5d0c98cd99c6a48aefadb88926e71fa5
Abhishek Mahato <abhishekmahato3171@gmail.com>
2026-05-26 23:24:54 +0530

    DataType

---
+++ JSdataType.js
@@ -0,0 +1,21 @@
+let name="Abhishek";
+let age=25;
+let isStudent=true;
+let hobbies=["coding", "gaming", "traveling"];
+let address={
+    city:"New York",
+    country:"USA"
+};
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
+console.log(address);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof address);
+console.log(typeof null);
+console.log(typeof undefined);
