9b9c574365c238b3a84481c2dd402940e780235a
Abhishek Mahato <abhishekmahato3171@gmail.com>
2026-05-26 23:45:22 +0530

    DataType Conversion

---
+++ DataConversion.js
@@ -0,0 +1,8 @@
+let score="33";   //score is a string
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let Conversion=Number(score); //score is converted to a number
+console.log(typeof Conversion);
+console.log(Conversion);
