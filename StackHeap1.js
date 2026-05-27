de51bfee7dc8cf3b2027a14d5659102610e7fe0f
Abhishek Mahato <abhishekmahato3171@gmail.com>
2026-05-27 12:18:12 +0530

    Hask & Heap Basic

---
+++ StackHeap1.js
@@ -0,0 +1,27 @@
+let myYouTubeName = "hiteshchoudhary.com";
+
+let anotherName = myYouTubeName;
+
+anotherName="AbhishekMahato";
+
+console.log(myYouTubeName);
+
+console.log(anotherName);
+
+
+
+let user1 = {
+
+email: "user@google.com",
+
+upi: "user@ybl"
+
+}
+
+let user2 = user1
+
+user2.email = "hitesh@google.com"
+
+console.log(user1.email);
+
+console.log(user2.email);
